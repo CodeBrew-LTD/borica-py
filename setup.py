5fc001b7720a2d5258fdc515d4d2d66c3dfbb32a
Kaloyan Krumov <kaloyankr@aiidatapro.com>
2021-04-30 15:41:24 +0300

    initial refactoring

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='borica-py',
+    # version='0.0.0',
+    packages=['borica'],
+    # url='',
+    # license='',
+    # author='CodeBrew',
+    # author_email='contact@codebrew.io',
+    # description='Python integration for Borica based payments',
+    # scripts=['bin/configure.py'],
+)
+
+# if __name__ == '__main__':
+#     # parser = argparse.ArgumentParser(description='A test program.')
+#     # args = parser.parse_args()
+#     setup()
